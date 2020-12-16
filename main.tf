c20d2cbace689c5834601f2fdf1977b5434b8141
Mahboob Ali <alimizan@hotmail.com>
2020-12-16 18:47:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ resource "azurerm_policy_definition" "OnTimeTag_policy" {
   mode                  = "Indexed"
   display_name          = "OnTime Tag Policy"
   management_group_name = var.definition_management_group
-  policy_rule           = file("${path.module}/policies/key-vault-soft-delete/policy-rule.json")
+  policy_rule           = file("${path.module}/policies/policy-rule.json")
 
 }
